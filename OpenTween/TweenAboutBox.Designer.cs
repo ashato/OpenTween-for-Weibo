a57805fce38f4cdb20b6d9401fb94461c7640eb9
unknown <yoshinaga@yoshinaga-PC.workslan>
2012-07-05 17:54:18 +0800

    test

--- OpenTween/TweenAboutBox.Designer.cs
+++ OpenTween/TweenAboutBox.Designer.cs
@@ -66,25 +66,25 @@
             // LabelProductName
             // 
             resources.ApplyResources(this.LabelProductName, "LabelProductName");
-            this.LabelProductName.MaximumSize = new System.Drawing.Size(0, 16);
+            this.LabelProductName.MaximumSize = new System.Drawing.Size(0, 17);
             this.LabelProductName.Name = "LabelProductName";
             // 
             // LabelVersion
             // 
             resources.ApplyResources(this.LabelVersion, "LabelVersion");
-            this.LabelVersion.MaximumSize = new System.Drawing.Size(0, 16);
+            this.LabelVersion.MaximumSize = new System.Drawing.Size(0, 17);
             this.LabelVersion.Name = "LabelVersion";
             // 
             // LabelCopyright
             // 
             resources.ApplyResources(this.LabelCopyright, "LabelCopyright");
-            this.LabelCopyright.MaximumSize = new System.Drawing.Size(0, 16);
+            this.LabelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
             this.LabelCopyright.Name = "LabelCopyright";
             // 
             // LabelCompanyName
             // 
             resources.ApplyResources(this.LabelCompanyName, "LabelCompanyName");
-            this.LabelCompanyName.MaximumSize = new System.Drawing.Size(0, 16);
+            this.LabelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
             this.LabelCompanyName.Name = "LabelCompanyName";
             // 
             // TextBoxDescription
